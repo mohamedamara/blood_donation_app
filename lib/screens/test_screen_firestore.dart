34b2981a9cae3800ae93b87a9bc613fd028b2491
Mohamed Amara <20605958+mohamedamara@users.noreply.github.com>
2019-10-14 14:00:43 +0100

    Fix multidex problem

--- lib/screens/test_screen_firestore.dart
+++ lib/screens/test_screen_firestore.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class TestScreenFirestore extends StatelessWidget {
@@ -7,13 +8,13 @@ class TestScreenFirestore extends StatelessWidget {
       body: Center(
         child: Container(
           color: Colors.amber,
-//          child: StreamBuilder(
-//            stream: Firestore.instance.collection("testCollection").snapshots(),
-//            builder: (context, snapshot) {
-//              if(!snapshot.hasData) return Text("loading...");
-//              return Text(snapshot.data.documents[0]["name"]);
-//            }
-//          ),
+         child: StreamBuilder(
+           stream: Firestore.instance.collection("testCollection").snapshots(),
+           builder: (context, snapshot) {
+             if(!snapshot.hasData) return Text("loading...");
+             return Text(snapshot.data.documents[0]["name"]);
+           }
+         ),
         ),
       ),
     );
